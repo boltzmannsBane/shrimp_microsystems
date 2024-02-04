b0d619db48f9ec025f2d3a9f7c3bd45a99221325
amygdala <leonapkhazava@gmail.com>
2024-02-04 22:56:51 +0400

    in the arena trying stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Person;
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("🦐".to_string())));
+    commands.spawn((Person, Name(" 🍤".to_string())));
+    commands.spawn((Person, Name("🐙".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
